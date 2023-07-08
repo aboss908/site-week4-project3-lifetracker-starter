8d7016fd0ab506bdca35b3abe53d8d685040f60b
aboss908 <aboss908@gmail.com>
2023-07-07 22:53:13 -0400

    Updating for render

--- lifetracker-api/server.js
+++ lifetracker-api/server.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 const {PORT} = require('./config/config')
 const morgan = require('morgan')
-const cors = require('cors')
 const auth = require('./routes/auth')
 const router = require('./routes/routes')
 const {NotFoundError} = require("./utils/errors")
 
-app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
