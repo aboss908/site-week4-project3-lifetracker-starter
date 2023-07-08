47835927df5c0c7c70709fee9b2b1a4494cb2433
aboss908 <aboss908@gmail.com>
2023-07-07 22:16:45 -0400

    Updating for render

--- lifetracker-api/config/config.js
+++ lifetracker-api/config/config.js
@@ -5,11 +5,11 @@ const BCRYPT_WORK_FACTOR = process.env.WORK_FACTOR || 10
 
 // Grabbing data to use as the connection-string for the client.
 function getDatabaseURI() {
-    const dbUser = process.env.DATABASE_USER || postgres
-    const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : postgres
-    const dbHost = process.env.DATABASE_HOST || postgres
-    const dbPort = process.env.DATABASE_PORT || 5432
-    const dbName = process.env.DATABASE_NAME || postgres
+    // const dbUser = process.env.DATABASE_USER || postgres
+    // const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : postgres
+    // const dbHost = process.env.DATABASE_HOST || postgres
+    // const dbPort = process.env.DATABASE_PORT || 5432
+    // const dbName = process.env.DATABASE_NAME || postgres
 
     return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
 }
